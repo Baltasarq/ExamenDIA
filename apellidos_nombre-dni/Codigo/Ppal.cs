32cd639bf5effe96da7e108a44426eb4798673b0
Baltasar <baltasarq@gmail.com>
2017-03-17 11:18:31 +0100

    Added the creation of 'notas.txt'

--- apellidos_nombre-dni/Codigo/Ppal.cs
+++ apellidos_nombre-dni/Codigo/Ppal.cs
@@ -1,10 +1,14 @@
 ﻿namespace Codigo {
 	using System;
+    using System.IO;
 
 	class Ppal {
+        public const string NotesFileName = "notas.txt";
+    
 		static void Main()
 		{
 			Console.WriteLine( "Nombre: " + Info.Nombre );
+            Console.WriteLine( "Email: " + Info.Email );
 			Console.WriteLine( "DNI: {0}{1}", Info.Dni, Info.LetraDni );
 			Console.WriteLine( "===" );
 
@@ -19,6 +23,25 @@
               || Info.Dni < 0 )
             {
                 Console.WriteLine( "\n** Recuerda escribir tus datos en Info.cs **" );
+            } else {
+                if ( !File.Exists( NotesFileName ) ) {
+                    using ( StreamWriter writer = File.CreateText( NotesFileName ) ) {
+                        writer.WriteLine( "Nombre: " + Info.Nombre );
+                        writer.WriteLine( "Email: " + Info.Email );
+                        writer.WriteLine( "DNI: {0}{1}", Info.Dni, Info.LetraDni );
+                        writer.WriteLine( "===" );
+                        writer.WriteLine( "\nEjercicio 1: " );
+                        writer.WriteLine( "Ejercicio 2: " );
+                        writer.WriteLine( "Ejercicio 3: " );
+                        writer.WriteLine( "Ejercicio 4: " );
+                        writer.WriteLine( "Ejercicio 5: " );
+                        writer.WriteLine( "Ejercicio 6: " );
+                        writer.WriteLine();
+                        writer.WriteLine( "Nota: " );
+                    }
+                    
+                    Console.WriteLine( "\n**Archivo '{0}' creado. **", NotesFileName );
+                }
             }
             
             Console.WriteLine( "\n" );
