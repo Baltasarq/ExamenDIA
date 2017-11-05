e7f923153f3a49508c63a9976ee83e8c707d9cc8
baltasarq <baltasarq@gmail.com>
2017-11-05 18:24:55 +0100

    Removed old project directory

--- apellidos_nombre-dni/Codigo/Info.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Codigo {
-	public static class Info {
-		public const string Nombre = "";
-        public const string Email = "";
-		public const int Dni = -1;
-		public const char LetraDni = ' ';
-	}
-}
