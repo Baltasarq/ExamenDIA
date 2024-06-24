3d1db0c99620d35f86d255616f9a8e18d636973e
baltasarq <baltasarq@gmail.com>
2024-06-24 11:03:31 +0200

    Question 1 scaffolding simplified

--- CodigoExamen/Pregunta1/Ppal.cs
+++ CodigoExamen/Pregunta1/Ppal.cs
@@ -11,7 +11,21 @@ namespace CodigoExamen.Pregunta1 {
 
 			Console.WriteLine( "\n\n" + NamespaceName.Substring( NamespaceName.LastIndexOf( '.' ) + 1 ) );
 			Console.WriteLine( "=========" );
-			Console.WriteLine( Pregunta1.Explicacion );
+			Console.WriteLine( @"
+				Esta es la plantilla para tu examen de DIA.
+
+				Debe funcionar tanto en Rider como en Visual Studio.
+				Si tienes que compartir fuentes entre preguntas
+				(p.ej., porque se pide ampliar una clase ya creada),
+				puedes hacerlo en la misma pregunta,
+				utilizando using y partial (si es necesario), o
+				incluso copiar la clase de la pregunta anterior
+				en la nueva.
+
+				Tienes creado aparte un proyecto para tests.
+
+				Buena suerte,
+				-- Baltasar García, jbgarcia@uvigo.es" );
 		}
 	}
 }
