aec089f62caa398e3d694082b07e9147b60fb1db
baltasarq <baltasarq@gmail.com>
2024-10-15 16:40:16 +0200

    Modernized source code

---
+++ CodigoExamen/Solucion/Ppal.cs
@@ -0,0 +1,32 @@
+﻿// ExamenDIA (c) 2021 Baltasar MIT License <baltasarq@uvigo.es>
+
+
+namespace CodigoExamen.Solucion;
+    
+using System;
+
+
+public static class Ppal {
+	public static void Prueba()
+	{
+		string NamespaceName = typeof( Ppal ).Namespace ?? "Solucion";
+
+		Console.WriteLine( "\n\n" + NamespaceName.Substring( NamespaceName.LastIndexOf( '.' ) + 1 ) );
+		Console.WriteLine( "=========" );
+		Console.WriteLine( @"
+			Esta es la plantilla para tu examen de DIA.
+
+			Debe funcionar tanto en Rider como en Visual Studio.
+			Si tienes que compartir fuentes entre preguntas
+			(p.ej., porque se pide ampliar una clase ya creada),
+			puedes hacerlo en la misma pregunta,
+			utilizando using y partial (si es necesario), o
+			incluso copiar la clase de la pregunta anterior
+			en la nueva.
+
+			Tienes creado aparte un proyecto para tests.
+
+			Buena suerte,
+			-- Baltasar García, jbgarcia@uvigo.es" );
+	}
+}
--- Tests/TestPpal.cs
+++ Tests/TestPpal.cs
@@ -1,16 +1,17 @@
 ﻿// ExamenDIA (c) 2021 Baltasar MIT License <baltasarq@uvigo.es>
 
 
-namespace Tests {
-	using CodigoExamen.Pregunta1;
-	using NUnit.Framework;
-	
-	[TestFixture]
-	public class TestPpal {
-		[Test]
-		public void TestTest()
-		{
-			Assert.Pass();
-		}
+namespace Tests;
+
+using CodigoExamen.Solucion;
+using NUnit.Framework;
+
+
+[TestFixture]
+public class TestPpal {
+	[Test]
+	public void TestTest()
+	{
+		Assert.Pass();
 	}
 }
